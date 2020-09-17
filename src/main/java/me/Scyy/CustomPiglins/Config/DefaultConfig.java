f24b67eba7d82d00e7e2d06376a1636aecc7d2fc
Scyy <bamslizard3201@gmail.com>
2020-09-17 20:33:52 +0800

    Created a class for the default config file

---
+++ src/main/java/me/Scyy/CustomPiglins/Config/DefaultConfig.java
@@ -0,0 +1,10 @@
+package me.Scyy.CustomPiglins.Config;
+
+import me.Scyy.CustomPiglins.Plugin;
+
+public class DefaultConfig extends ConfigFile {
+
+    public DefaultConfig(Plugin plugin) {
+        super(plugin, "config.yml");
+    }
+}
