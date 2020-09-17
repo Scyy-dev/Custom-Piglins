c9129bf42c1eb581bdd67a388a11a62ab651379e
Scyy <bamslizard3201@gmail.com>
2020-09-17 20:30:52 +0800

    Created a CustomPiglinLootGenerator class, which holds an array of PiglinItems and randomly picks one

---
+++ src/main/java/me/Scyy/CustomPiglins/CustomPiglinLootGenerator.java
@@ -0,0 +1,50 @@
+package me.Scyy.CustomPiglins;
+
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class CustomPiglinLootGenerator {
+
+    private final ArrayList<PiglinItem> itemWeighting;
+
+    private final Plugin plugin;
+
+    private final Random random;
+
+    public CustomPiglinLootGenerator(Plugin plugin) {
+
+        this.plugin = plugin;
+        this.itemWeighting = plugin.getConfigFileHandler().loadWeightings();
+        this.random = new Random();
+
+    }
+
+    public ItemStack generateItem() {
+
+        // Get a random value from the array
+        int randomIndex = random.nextInt(itemWeighting.size());
+
+        // Get the random value in the array
+        PiglinItem piglinItem = itemWeighting.get(randomIndex);
+
+        // Get the ItemStack from the array
+        ItemStack item = itemWeighting.get(randomIndex).getItem();
+
+        // Calculate the random quantity
+        int randomAmount = random.nextInt(piglinItem.getMaxAmount() - piglinItem.getMinAmount()) + piglinItem.getMinAmount();
+
+        // Assign the random amount
+        item.setAmount(randomAmount);
+
+        // Return the item
+        return item;
+
+    }
+
+    public ArrayList<PiglinItem> getItemWeighting() {
+        return itemWeighting;
+    }
+
+}
