ced70270f92ece27da3e07cd6c8f03d0374eacd2
Scyy <bamslizard3201@gmail.com>
2020-09-17 21:27:46 +0800

    Separated the PiglinItem array into a hashmap of the item and an identifier, as well as an array that stores the number of identifiers as the weighting specifies

--- src/main/java/me/Scyy/CustomPiglins/Piglins/CustomPiglinLootGenerator.java
+++ src/main/java/me/Scyy/CustomPiglins/Piglins/CustomPiglinLootGenerator.java
@@ -1,37 +1,49 @@
 package me.Scyy.CustomPiglins.Piglins;
 
+import me.Scyy.CustomPiglins.Config.ItemWeightingsConfig;
 import me.Scyy.CustomPiglins.Plugin;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.*;
 
 public class CustomPiglinLootGenerator {
 
-    private final ArrayList<PiglinItem> itemWeighting;
+    private Map<Integer, PiglinItem> piglinItems;
 
-    private final Plugin plugin;
+    private final ArrayList<Integer> rawWeightings;
 
     private final Random random;
 
     public CustomPiglinLootGenerator(Plugin plugin) {
 
-        this.plugin = plugin;
-        this.itemWeighting = plugin.getConfigFileHandler().loadWeightings();
+        ItemWeightingsConfig weightingsConfig = plugin.getConfigFileHandler().getItemWeightingsConfig();
+        this.piglinItems = weightingsConfig.loadWeightings();
         this.random = new Random();
+        this.rawWeightings = new ArrayList<>();
+
+        for (PiglinItem piglinItem : piglinItems.values()) {
+
+            for (int i = 0; i < piglinItem.getWeight(); i++) {
+
+                rawWeightings.add(piglinItem.getItemID());
+
+            }
+
+        }
 
     }
 
     public ItemStack generateItem() {
 
         // Get a random value from the array
-        int randomIndex = random.nextInt(itemWeighting.size());
+        int randomIndex = random.nextInt(piglinItems.size());
 
         // Get the random value in the array
-        PiglinItem piglinItem = itemWeighting.get(randomIndex);
+        int piglinItemID = rawWeightings.get(randomIndex);
+        PiglinItem piglinItem = piglinItems.get(piglinItemID);
 
         // Get the ItemStack from the array
-        ItemStack item = itemWeighting.get(randomIndex).getItem();
+        ItemStack item = piglinItems.get(randomIndex).getItem();
 
         // Calculate the random quantity
         int randomAmount = random.nextInt(piglinItem.getMaxAmount() - piglinItem.getMinAmount()) + piglinItem.getMinAmount();
@@ -44,8 +56,27 @@ public class CustomPiglinLootGenerator {
 
     }
 
-    public ArrayList<PiglinItem> getItemWeighting() {
-        return itemWeighting;
+    public Collection<PiglinItem> getPiglinItems() {
+        return piglinItems.values();
     }
 
+    public void updatePiglinItems(Map<Integer, PiglinItem> newItems) {
+
+        // Replace the map
+        piglinItems = newItems;
+
+        // Empty the rawWeightings array
+
+        // Iterate over the new items and update the weighting
+        for (PiglinItem piglinItem : piglinItems.values()) {
+
+            for (int weight = 0; weight < piglinItem.getWeight(); weight++) {
+
+                rawWeightings.add(piglinItem.getItemID());
+
+            }
+
+        }
+
+    }
 }
