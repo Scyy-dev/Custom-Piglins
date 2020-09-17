57411694429fb95d6d0374ce8a7c3642426fed21
Scyy <bamslizard3201@gmail.com>
2020-09-17 21:37:44 +0800

    Registered the InventoryClick listener in the plugin class

--- src/main/java/me/Scyy/CustomPiglins/GUI/CustomPiglinGUIListener.java
+++ src/main/java/me/Scyy/CustomPiglins/GUI/CustomPiglinGUIListener.java
@@ -1,9 +1,18 @@
 package me.Scyy.CustomPiglins.GUI;
 
+import me.Scyy.CustomPiglins.Plugin;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 
 public class CustomPiglinGUIListener implements Listener {
+
+    private final Plugin plugin;
+
+    public CustomPiglinGUIListener(Plugin plugin) {
+
+        this.plugin = plugin;
+
+    }
 
     public void onInventoryClickEvent(InventoryClickEvent event) {
 
--- src/main/java/me/Scyy/CustomPiglins/Plugin.java
+++ src/main/java/me/Scyy/CustomPiglins/Plugin.java
@@ -1,6 +1,7 @@
 package me.Scyy.CustomPiglins;
 
 import me.Scyy.CustomPiglins.Config.ConfigFileHandler;
+import me.Scyy.CustomPiglins.GUI.CustomPiglinGUIListener;
 import me.Scyy.CustomPiglins.Piglins.CustomPiglinLootGenerator;
 import me.Scyy.CustomPiglins.Piglins.PiglinItemDropEvent;
 import org.bukkit.Bukkit;
@@ -28,6 +29,9 @@ public class Plugin extends JavaPlugin {
 
         // Register the item drop listener
         Bukkit.getPluginManager().registerEvents(new PiglinItemDropEvent(generator, this), this);
+
+        // Register the inventory listener
+        Bukkit.getPluginManager().registerEvents(new CustomPiglinGUIListener(this), this);
 
     }
 
