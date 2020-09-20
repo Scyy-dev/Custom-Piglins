68013bba69a27cc7c19ae2d6747ab0f09ef2862f
Scyy <bamslizard3201@gmail.com>
2020-09-20 10:56:49 +0800

    Modified the way Piglin Items are loaded from config to prevent the ItemStack being null as well as correctly identifying the ID

--- src/main/java/me/Scyy/CustomPiglins/Config/ItemWeightingsConfig.java
+++ src/main/java/me/Scyy/CustomPiglins/Config/ItemWeightingsConfig.java
@@ -4,6 +4,7 @@ import me.Scyy.CustomPiglins.Piglins.PiglinItem;
 import me.Scyy.CustomPiglins.Plugin;
 import org.bukkit.inventory.ItemStack;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,34 +24,36 @@ public class ItemWeightingsConfig extends ConfigFile {
 
         boolean sendWarning = false;
 
-        if (config.getList("items") == null) {
+        if (config.getConfigurationSection("items") == null) {
 
             plugin.getLogger().warning("Unable to load any items! Piglins will not drop anything!");
             return new HashMap<>();
 
         }
 
-        int itemCount = config.getList("items").size();
+        if (config.getConfigurationSection("items").getKeys(false).size() == 0) {
+
+            plugin.getLogger().warning("No items found in items.yml! Piglins will not drop anything!");
+            return new HashMap<>();
+
+        }
 
         Map<Integer, PiglinItem> weightings = new HashMap<>();
 
-        int itemID = 0;
+        for (String key : config.getConfigurationSection("items").getKeys(false)) {
 
-        for (int itemIndex = 0; itemIndex < itemCount; itemIndex++) {
+            int weight = config.getInt("items." + key + ".weight", -1);
+            int itemID = Integer.parseInt(key);
+            ItemStack item = ItemStack.deserialize(config.getConfigurationSection("items." + key + ".item").getValues(true));
+            int minAmount = config.getInt("items." + key + ".min", -1);
+            int maxAmount = config.getInt("items." + key + ".max", -1);
 
-            int weight = config.getInt("items." + itemIndex + ".weight", -1);
-            ItemStack item = config.getItemStack("items." + itemIndex + ".item");
-            int minAmount = config.getInt("items." + itemIndex + ".min", -1);
-            int maxAmount = config.getInt("items." + itemIndex + ".max", -1);
-
-            if (weight == -1 || item == null || minAmount == -1 || maxAmount == -1) {
+            if (weight == -1 || minAmount == -1 || maxAmount == -1) {
                 sendWarning = true;
                 break;
             }
 
             weightings.put(itemID, new PiglinItem(item, itemID, weight, minAmount, maxAmount));
-
-            itemID++;
 
         }
 
