74ca50bbfeb5f8fa9fa108fde526436c618b6dde
Scyy <bamslizard3201@gmail.com>
2020-09-21 19:06:59 +0800

    Adjusted the CustomPiglinGUIListener to suit Inventory change

--- src/main/java/me/Scyy/CustomPiglins/GUI/CustomPiglinGUIListener.java
+++ src/main/java/me/Scyy/CustomPiglins/GUI/CustomPiglinGUIListener.java
@@ -1,5 +1,6 @@
 package me.Scyy.CustomPiglins.GUI;
 
+import me.Scyy.CustomPiglins.Piglins.CustomPiglinLootGenerator;
 import me.Scyy.CustomPiglins.Plugin;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,16 +10,19 @@ public class CustomPiglinGUIListener implements Listener {
 
     private final Plugin plugin;
 
+    private final CustomPiglinLootGenerator generator;
+
     public CustomPiglinGUIListener(Plugin plugin) {
 
         this.plugin = plugin;
+        this.generator = plugin.getGenerator();
 
     }
 
     @EventHandler
     public void onInventoryClickEvent(InventoryClickEvent event) {
 
-        if (!(event.getView().getTitle().equalsIgnoreCase(CustomPiglinGUI.INVENTORY_NAME))) return;
+        if (!(event.getView().getTitle().equalsIgnoreCase(InventoryGUI.INVENTORY_NAME))) return;
 
         event.setCancelled(true);
 
