2d365a4e9a481bcdbe38f9dc0b39468b830333c9
Scyy <bamslizard3201@gmail.com>
2020-09-21 20:02:51 +0800

    Renamed PiglinItemGUI to PiglinItemListGUI to make room for the more appropriately named PiglinItemGUI, which is the GUI for individual piglin items

--- src/main/java/me/Scyy/CustomPiglins/GUI/PiglinItemListGUI.java
+++ src/main/java/me/Scyy/CustomPiglins/GUI/PiglinItemListGUI.java
@@ -3,6 +3,8 @@ package me.Scyy.CustomPiglins.GUI;
 import me.Scyy.CustomPiglins.Piglins.CustomPiglinLootGenerator;
 import me.Scyy.CustomPiglins.Piglins.PiglinItem;
 import me.Scyy.CustomPiglins.Plugin;
+import me.Scyy.CustomPiglins.Util.ItemBuilder;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
@@ -11,12 +13,15 @@ import java.util.ArrayList;
 // # 0 0 0 0 0 0 0 #
 // # 0 0 0 0 0 0 0 #
 // # 0 0 0 0 0 0 0 #
-// # # # # # # # # #
-// # # # # # # # # #
+// # P # # # # # N #
+// # # # # S # # # #
+// P = previous page
+// N = next page
+// S = settings
 
-public class PiglinItemGUI extends InventoryGUI {
+public class PiglinItemListGUI extends InventoryGUI {
 
-    public PiglinItemGUI(int page, Player player, Plugin plugin) {
+    public PiglinItemListGUI(int page, Player player, Plugin plugin) {
         super(page, player, plugin);
 
         // Get the generator from the UI
@@ -31,31 +36,46 @@ public class PiglinItemGUI extends InventoryGUI {
         // Calculate the ending index
         int piglinItemEndIndex = 21 * (page + 1);
 
-        // Check if the end of the piglin items has been reached
-        if (piglinItemEndIndex > piglinItems.size()) piglinItemEndIndex = piglinItems.size();
-
-        // Check if the page number is greater than the number of pages available
-        if (piglinItemStartIndex > piglinItems.size()) piglinItemStartIndex = piglinItemEndIndex;
-
         // inventory index tracks the inventory slot of each item, is irrelevant to the index for in piglinItems
         int inventoryIndex = 10;
 
         // Iterate over the defined range and add each item to the inventory
         for (int itemIndex = piglinItemStartIndex; itemIndex < piglinItemEndIndex; itemIndex++) {
 
-            // Get the Piglin Item
-            PiglinItem piglinItem = piglinItems.get(itemIndex);
+            // Put the item in the array if it is accessible, otherwise add air
+            if (itemIndex < piglinItems.size()) {
 
-            // Put the item in the array
-            inventoryItems[inventoryIndex] = piglinItem.getItem();
+                // Get the Piglin Item
+                PiglinItem piglinItem = piglinItems.get(itemIndex);
+
+                // Add it the inventory array
+                inventoryItems[inventoryIndex] = piglinItem.getItem();
+
+            } else {
+
+                // Add an empty value to the inventory array
+                inventoryItems[inventoryIndex] = null;
+
+            }
 
             // Increment the inventory index
             inventoryIndex++;
 
-            // Check if the inventory slot is at the edge, and move it over
+            // Check if the inventory slot is one from the edge, and move index to other side
             if ((inventoryIndex - 8) % 9 == 0) inventoryIndex += 2;
 
         }
+
+        // Check if the page is not 0 and if so add the previous pagination arrow
+        if (page != 0) {
+
+            inventoryItems[37] = new ItemBuilder(Material.ARROW).name("&6Previous").build();
+            new ItemBuilder(inventoryItems[5]);
+
+        }
+
+        // Add the next pagination arrow
+        inventoryItems[43] = new ItemBuilder(Material.ARROW).name("&6Next").build();
 
         // Assign the inventory items to the inventory
         inventory.setContents(inventoryItems);
