a4cc6784436a9c4d16d7053bc234d83418ae169b
Scyy <bamslizard3201@gmail.com>
2020-09-22 19:14:00 +0800

    Added check based in InventoryHolder rather than name in CustomPiglinGUIListener to prevent named chests from creating inventory behaviour

--- src/main/java/me/Scyy/CustomPiglins/GUI/CustomPiglinGUIListener.java
+++ src/main/java/me/Scyy/CustomPiglins/GUI/CustomPiglinGUIListener.java
@@ -1,10 +1,16 @@
 package me.Scyy.CustomPiglins.GUI;
 
 import me.Scyy.CustomPiglins.Piglins.CustomPiglinLootGenerator;
+import me.Scyy.CustomPiglins.Piglins.PiglinItem;
 import me.Scyy.CustomPiglins.Plugin;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
 
 public class CustomPiglinGUIListener implements Listener {
 
@@ -22,8 +28,13 @@ public class CustomPiglinGUIListener implements Listener {
     @EventHandler
     public void onInventoryClickEvent(InventoryClickEvent event) {
 
-        if (!(event.getView().getTitle().equalsIgnoreCase(InventoryGUI.INVENTORY_NAME))) return;
+        // Check if the click was a null click
+        if (event.getClickedInventory() == null) return;
 
+        // Check if the inventory clicked is an inventory defined by this plugin
+        if (!(event.getClickedInventory().getHolder() instanceof InventoryGUI)) return;
+
+        // Cancel the event
         event.setCancelled(true);
 
 
