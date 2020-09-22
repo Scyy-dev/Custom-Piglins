b5d0e4ab65ffc2b2ce66bf09e55eb2539fca39f6
Scyy <bamslizard3201@gmail.com>
2020-09-22 18:58:45 +0800

    Created GUIContext class, responsible for holding data between inventory events so that the correct information can be displayed

---
+++ src/main/java/me/Scyy/CustomPiglins/GUI/GUIContext.java
@@ -0,0 +1,43 @@
+package me.Scyy.CustomPiglins.GUI;
+
+import me.Scyy.CustomPiglins.Piglins.PiglinItem;
+import org.bukkit.entity.Player;
+
+public class GUIContext {
+
+    private PiglinItem piglinItem = null;
+
+    private Player player;
+
+    private int page;
+
+    public GUIContext(PiglinItem piglinItem, Player player, int page) {
+        this.piglinItem = piglinItem;
+        this.player = player;
+        this.page = page;
+    }
+
+    public PiglinItem getPiglinItem() {
+        return piglinItem;
+    }
+
+    public void setPiglinItem(PiglinItem piglinItem) {
+        this.piglinItem = piglinItem;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+}
