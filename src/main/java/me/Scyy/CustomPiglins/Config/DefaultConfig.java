34fc88cc66fc7976a0c54c5624f6be51cd0f1723
Scyy <bamslizard3201@gmail.com>
2020-09-25 20:29:16 +0800

    Added method to obtain information about the piglin converter from the DefaultConfig

--- src/main/java/me/Scyy/CustomPiglins/Config/DefaultConfig.java
+++ src/main/java/me/Scyy/CustomPiglins/Config/DefaultConfig.java
@@ -1,10 +1,47 @@
 package me.Scyy.CustomPiglins.Config;
 
 import me.Scyy.CustomPiglins.Plugin;
+import me.Scyy.CustomPiglins.Util.ItemBuilder;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.List;
 
 public class DefaultConfig extends ConfigFile {
 
+    private final ItemStack piglinConverter;
+
     public DefaultConfig(Plugin plugin) {
         super(plugin, "config.yml");
+
+        // Get all the data from config
+        Material pcMaterial = Material.getMaterial(config.getString("piglinConverter.material", "BARRIER"));
+        String rawName = config.getString("piglinConverter.name", "NAME_NOT_FOUND");
+        List<String> rawLore = config.getStringList("piglinConverter.lore");
+        boolean isShiny = config.getBoolean("piglinConverter.isShiny", false);
+
+        // Create the item builder
+        ItemBuilder pcBuilder = new ItemBuilder(pcMaterial);
+
+        // Provide the name
+        pcBuilder.name(ChatColor.translateAlternateColorCodes('&', rawName));
+
+        // Provide the lore
+        for (String lore : rawLore) {
+
+            pcBuilder.lore(ChatColor.translateAlternateColorCodes('&', lore));
+
+        }
+
+        // Provide the enchantments
+        if (isShiny) pcBuilder.enchant();
+
+        // Create the item
+        piglinConverter = pcBuilder.build();
+    }
+
+    public ItemStack getPiglinConverter() {
+        return piglinConverter;
     }
 }
