e0bc63b226611bd2f9833347a996ae84c52f0944
Scyy <bamslizard3201@gmail.com>
2020-09-25 19:20:18 +0800

    Updated the toString() method for PiglinItem to include whether it has random damage

--- src/main/java/me/Scyy/CustomPiglins/Piglins/PiglinItem.java
+++ src/main/java/me/Scyy/CustomPiglins/Piglins/PiglinItem.java
@@ -83,6 +83,7 @@ public class PiglinItem {
                 ", weight=" + weight +
                 ", minAmount=" + minAmount +
                 ", maxAmount=" + maxAmount +
+                ", hasRD=" + randomDamage +
                 '}';
     }
 
