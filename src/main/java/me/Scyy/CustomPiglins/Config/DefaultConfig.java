62db492d971fd062b210c74ae108237452dc20b9
Scyy <bamslizard3201@gmail.com>
2020-09-28 13:38:36 +0800

    Updated the DefaultConfig to handle both the consumable and non-consumable piglin converter

--- src/main/java/me/Scyy/CustomPiglins/Config/DefaultConfig.java
+++ src/main/java/me/Scyy/CustomPiglins/Config/DefaultConfig.java
@@ -10,38 +10,71 @@ import java.util.List;
 
 public class DefaultConfig extends ConfigFile {
 
-    private final ItemStack piglinConverter;
+    private final ItemStack consumableConverter;
+
+    private final ItemStack nonConsumableConverter;
 
     public DefaultConfig(Plugin plugin) {
         super(plugin, "config.yml");
 
-        // Get all the data from config
-        Material pcMaterial = Material.getMaterial(config.getString("piglinConverter.material", "BARRIER"));
-        String rawName = config.getString("piglinConverter.name", "NAME_NOT_FOUND");
-        List<String> rawLore = config.getStringList("piglinConverter.lore");
-        boolean isShiny = config.getBoolean("piglinConverter.isShiny", false);
+        // Get the materials from config
+        Material cPCMaterial = Material.getMaterial(config.getString("piglinConverter.consumable.material"));
+        Material ncPCMaterial = Material.getMaterial(config.getString("piglinConverter.non-consumable.material"));
 
-        // Create the item builder
-        ItemBuilder pcBuilder = new ItemBuilder(pcMaterial);
+        // Check if the materials are the same
+        if (cPCMaterial == ncPCMaterial) {
 
-        // Provide the name
-        pcBuilder.name(ChatColor.translateAlternateColorCodes('&', rawName));
-
-        // Provide the lore
-        for (String lore : rawLore) {
-
-            pcBuilder.lore(ChatColor.translateAlternateColorCodes('&', lore));
+            throw new IllegalArgumentException("Piglin Converter items must be different materials!");
 
         }
 
-        // Provide the enchantments
-        if (isShiny) pcBuilder.enchant();
+        // Get the raw names from config
+        String cPCRawName = config.getString("piglinConverter.consumable.name", "NAME_NOT_FOUND");
+        String ncPCRawName = config.getString("piglinConverter.non-consumable.name", "NAME_NOT_FOUND");
 
-        // Create the item
-        piglinConverter = pcBuilder.build();
+        // Get the lore from config
+        List<String> cPCRawLore = config.getStringList("piglinConverter.consumable.lore");
+        List<String> ncPCRawLore = config.getStringList("piglinConverter.non-consumable.lore");
+
+        // get the shiny factor from config
+        boolean cPCIsShiny = config.getBoolean("piglinConverter.consumable.isShiny", false);
+        boolean ncPCIsShiny = config.getBoolean("piglinConverter.non-consumable.isShiny", false);
+
+        // Create the item builders
+        ItemBuilder cPCBuilder = new ItemBuilder(cPCMaterial);
+        ItemBuilder ncPCBuilder = new ItemBuilder(ncPCMaterial);
+
+        // Provide the names
+        cPCBuilder.name(ChatColor.translateAlternateColorCodes('&', cPCRawName));
+        ncPCBuilder.name(ChatColor.translateAlternateColorCodes('&', ncPCRawName));
+
+        // Provide the lore
+        for (String lore : cPCRawLore) {
+
+            cPCBuilder.lore(ChatColor.translateAlternateColorCodes('&', lore));
+
+        }
+        for (String lore : ncPCRawLore) {
+
+            ncPCBuilder.lore(ChatColor.translateAlternateColorCodes('&', lore));
+
+        }
+
+        // Provide the shiny factor
+        if (cPCIsShiny) cPCBuilder.enchant();
+        if (ncPCIsShiny) ncPCBuilder.enchant();
+
+        // Create the items
+        consumableConverter = cPCBuilder.build();
+        nonConsumableConverter = ncPCBuilder.build();
+
     }
 
-    public ItemStack getPiglinConverter() {
-        return piglinConverter;
+    public ItemStack getConsumableConverter() {
+        return consumableConverter;
+    }
+
+    public ItemStack getNonConsumableConverter() {
+        return nonConsumableConverter;
     }
 }
